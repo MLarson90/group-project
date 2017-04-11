af47c77d9a7c4f53a1beeba9d44dfb3d8c85a4db
Xing Li <msuli_1120@ymail.com>
2017-04-11 10:37:05 -0700

    add profile

--- app/app.php
+++ app/app.php
@@ -11,6 +11,7 @@
   $app = new Silex\Application();
   $DB = new PDO('mysql:host=localhost;dbname=appdata', 'root', 'root');
   $app['debug'] = true;
+
   $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
   ));
@@ -26,17 +27,21 @@
     {
       $new_user = new User($_POST['user_email'], $_POST['password']);
       $new_user->save();
-      return $app['twig']->render('profile.html.twig', array('user_id'=>$new_user->getId()));
+      return $app['twig']->render('profile.html.twig', array('user_id'=>$new_user->getId(), 'msg'=>''));
     } else {
       return $app['twig']->render('create_account.html.twig', array('msg'=>'Your passwords need to be the same'));
     }
   });
   $app->post("/homepage", function() use ($app) {
     //$new_user = User::findUserbyId($_POST['user_id']);
-    $new_profile = new Profile ($_POST['first_name'], $_POST['last_name'], $_POST['profile_pic'], $_POST['bio']);
-    var_dump($new_profile->getPicture());
-    $new_profile->save();
-    return $app['twig']->render('homepage.html.twig');
+    if(isset($_POST['button'])){
+      $new_profile = new Profile($_POST['first_name'], $_POST['last_name'], $_POST['profile_pic'], $_POST['bio']);
+      $new_profile->save($new_profile->getFirstName(), $new_profile->getLastName(), $new_profile->getBio(), $new_profile->getPicture());
+      return $app['twig']->render('homepage.html.twig', array('profile'=>Profile::findProfile($new_profile->getId())));
+    } else {
+        return $app['twig']->render('profile.html.twig', array('user_id'=>$_POST['user_id'], 'msg'=>''));
+    }
+
   });
   $app->post("/login_user", function() use ($app) {
     return $app['twig']->render('homepage.html.twig');
